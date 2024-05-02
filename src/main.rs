4a91f0952bde9f26aa95171731e95006c2c68132
Stefan Lüdin <stefan.luedin@digitecgalaxus.ch>
2024-05-02 16:59:33 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,161 @@
+mod tui;
+
+use std::time::Duration;
+
+use color_eyre::eyre::Result;
+use crossterm::event::KeyCode::Char;
+use ratatui::{prelude::*, widgets::*};
+use tokio::sync::mpsc::{self, UnboundedSender};
+use tui::Event;
+
+// App state
+struct App {
+    counter: i64,
+    should_quit: bool,
+    action_tx: UnboundedSender<Action>,
+}
+
+// App actions
+#[derive(Clone)]
+pub enum Action {
+    Tick,
+    Increment,
+    Decrement,
+    NetworkRequestAndThenIncrement, // new
+    NetworkRequestAndThenDecrement, // new
+    Quit,
+    Render,
+    None,
+}
+
+// App ui render function
+fn ui(f: &mut Frame, app: &mut App) {
+    let area = f.size();
+    f.render_widget(
+        Paragraph::new(format!(
+            "Press j or k to increment or decrement.\n\nCounter: {}",
+            app.counter,
+        ))
+        .block(
+            Block::default()
+                .title("ratatui async counter app")
+                .title_alignment(Alignment::Center)
+                .borders(Borders::ALL)
+                .border_type(BorderType::Rounded),
+        )
+        .style(Style::default().fg(Color::Cyan))
+        .alignment(Alignment::Center),
+        area,
+    );
+}
+
+fn get_action(_app: &App, event: Event) -> Action {
+    match event {
+        Event::Error => Action::None,
+        Event::Tick => Action::Tick,
+        Event::Render => Action::Render,
+        Event::Key(key) => {
+            match key.code {
+                Char('j') => Action::Increment,
+                Char('k') => Action::Decrement,
+                Char('J') => Action::NetworkRequestAndThenIncrement, // new
+                Char('K') => Action::NetworkRequestAndThenDecrement, // new
+                Char('q') => Action::Quit,
+                _ => Action::None,
+            }
+        }
+        _ => Action::None,
+    }
+}
+
+fn update(app: &mut App, action: Action) -> Option<Action> {
+    match action {
+        Action::Increment => {
+            app.counter += 1;
+        }
+        Action::Decrement => {
+            app.counter -= 1;
+        }
+        Action::NetworkRequestAndThenIncrement => {
+            let tx = app.action_tx.clone();
+            tokio::spawn(async move {
+                tokio::time::sleep(Duration::from_secs(5)).await; // simulate network request
+                tx.send(Action::Increment).unwrap();
+            });
+        }
+        Action::NetworkRequestAndThenDecrement => {
+            let tx = app.action_tx.clone();
+            tokio::spawn(async move {
+                tokio::time::sleep(Duration::from_secs(1)).await; // simulate network request
+                tx.send(Action::Decrement).unwrap();
+            });
+        }
+        Action::Quit => app.should_quit = true,
+        _ => {}
+    };
+
+    None
+}
+
+async fn run() -> Result<()> {
+    let (action_tx, mut action_rx) = mpsc::unbounded_channel(); // new
+
+    // ratatui terminal
+    let mut tui = tui::Tui::new()?.tick_rate(1.0).frame_rate(30.0);
+    tui.enter()?;
+
+    // application state
+    let mut app = App {
+        counter: 0,
+        should_quit: false,
+        action_tx: action_tx.clone(),
+    };
+
+    loop {
+        let e = tui.next().await.unwrap();
+        match e {
+            tui::Event::Quit => action_tx.send(Action::Quit)?,
+            tui::Event::Tick => action_tx.send(Action::Tick)?,
+            tui::Event::Render => action_tx.send(Action::Render)?,
+            tui::Event::Key(_) => {
+                let action = get_action(&app, e);
+                action_tx.send(action.clone())?;
+            }
+            _ => {}
+        };
+
+        while let Ok(action) = action_rx.try_recv() {
+            let mut maybe_action = Some(action);
+
+            // application update
+            while let Some(act) = maybe_action {
+                let next_action = update(&mut app, act.clone());
+                // render only when we receive Action::Render
+                if let Action::Render = act {
+                    tui.draw(|f| {
+                        ui(f, &mut app);
+                    })?;
+                }
+                maybe_action = next_action;
+            }
+        }
+
+        // application exit
+        if app.should_quit {
+            break;
+        }
+    }
+
+    tui.exit()?;
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let result = run().await;
+
+    result?;
+
+    Ok(())
+}
